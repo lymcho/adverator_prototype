b2ea8bd17061a9f4022de87a862251a3f5887828
ling zhou <lingzhoudesign@gmail.com>
2017-10-12 10:29:29 -0400

    js update
    js update

--- js/main.js
+++ js/main.js
@@ -22,18 +22,3 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 	after: function () {} // Callback to run after scroll
 });
 
-var scroll = new SmoothScroll('a[href*="#back"]', {
-	// Selectors
-	ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
-	header: null, // Selector for fixed headers (must be a valid CSS selector)
-
-	// Speed & Easing
-	speed: 800, // Integer. How fast to complete the scroll in milliseconds
-	offset: 40, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
-	easing: 'easeInOutCubic', // Easing pattern to use
-	customEasing: function (time) {}, // Function. Custom easing pattern
-
-	// Callback API
-	before: function () {}, // Callback to run before scroll
-	after: function () {} // Callback to run after scroll
-});
